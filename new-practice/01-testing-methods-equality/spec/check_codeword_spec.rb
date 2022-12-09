fe7fdc576294b4909185a18d865f03e92cb203d0
Sarita <saritaradia65@gmail.com>
2022-12-09 15:00:07 +0000

    Green test: Returns 'WRONG!' when the codeword is anything else

--- new-practice/01-testing-methods-equality/spec/check_codeword_spec.rb
+++ new-practice/01-testing-methods-equality/spec/check_codeword_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe "#check_codeword" do
     it "Returns 'Close, but nope.' when the codeword starts with 'h' and ends with 'e'" do
         expect(check_codeword("house")).to eq("Close, but nope.")
     end 
+
+    it "Returns 'WRONG!' when the codeword is anything else" do
+        expect(check_codeword("dog")).to eq("WRONG!")
+    end
 end
