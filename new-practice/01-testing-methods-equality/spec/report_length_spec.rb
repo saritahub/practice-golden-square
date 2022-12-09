3f9ec2a056173f01e05919b119309f3a66369614
Sarita <saritaradia65@gmail.com>
2022-12-09 15:07:06 +0000

    Green test: Includes numbers: Counts the number of characters in a string, and returns the value

--- new-practice/01-testing-methods-equality/spec/report_length_spec.rb
+++ new-practice/01-testing-methods-equality/spec/report_length_spec.rb
@@ -4,4 +4,7 @@ RSpec.describe "Report length" do
     it "Counts the number of characters in a string, and returns the value" do 
         expect(report_length("Baked Beans")).to eq("This string was 11 characters long.")
     end 
+    it "Includes numbers: Counts the number of characters in a string, and returns the value" do 
+        expect(report_length("This includes some numb3rs")).to eq("This string was 26 characters long.")
+    end 
 end 
