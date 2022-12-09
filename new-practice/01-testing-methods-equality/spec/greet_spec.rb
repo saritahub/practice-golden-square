a80520be5c44f95be4465834ad310592269a714d
Sarita <saritaradia65@gmail.com>
2022-12-09 14:56:05 +0000

    Green test: Greets the Cow by their name

--- new-practice/01-testing-methods-equality/spec/greet_spec.rb
+++ new-practice/01-testing-methods-equality/spec/greet_spec.rb
@@ -1,7 +1,10 @@
 require 'greet'
 
 RSpec.describe "greet" do
-    it "Greets the user by name" do 
+    it "Greets the Max by their name" do 
         expect(greet("Max")).to eq("Hello, Max")
+    end
+    it "Greets the Cow by their name" do 
+        expect(greet("Cow")).to eq("Hello, Cow")
     end
 end 
