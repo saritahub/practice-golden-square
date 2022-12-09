814d2107f07bfdcd6b94bba3a212f3422c0e2dc8
Sarita <saritaradia65@gmail.com>
2022-12-09 15:40:15 +0000

    Green test: Returns a gratitude list with one item

--- new-practice/02-testing-classes/spec/gratitudes_spec.rb
+++ new-practice/02-testing-classes/spec/gratitudes_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe "Gratitudes" do
         gratitudes = Gratitudes.new
         expect(gratitudes.format).to eq("Be grateful for: ")
     end  
+
+    it "Returns a gratitude list with one item" do
+        gratitudes = Gratitudes.new
+        gratitudes.add("Baked beans")
+        expect(gratitudes.format).to eq("Be grateful for: Baked beans")
+    end  
 end 
