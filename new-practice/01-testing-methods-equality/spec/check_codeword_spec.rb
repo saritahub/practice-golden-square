bbcde62f178fcbd8cf5cd24482a50d79991c1ad7
Sarita <saritaradia65@gmail.com>
2022-12-09 14:59:37 +0000

    Green test: Returns 'Close, but nope.' when the codeword starts with 'h' and ends with 'e'

--- new-practice/01-testing-methods-equality/spec/check_codeword_spec.rb
+++ new-practice/01-testing-methods-equality/spec/check_codeword_spec.rb
@@ -1,8 +1,11 @@
 require 'check_codeword'
 
 RSpec.describe "#check_codeword" do
-    it "returns 'Correct! Come in.' when the codeword is 'horse'" do
+    it "Returns 'Correct! Come in.' when the codeword is 'horse'" do
         expect(check_codeword("horse")).to eq("Correct! Come in.")
     end
 
+    it "Returns 'Close, but nope.' when the codeword starts with 'h' and ends with 'e'" do
+        expect(check_codeword("house")).to eq("Close, but nope.")
+    end 
 end
