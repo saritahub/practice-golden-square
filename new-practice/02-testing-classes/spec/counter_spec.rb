78e5e0efbcf19eb8def867e53ac3ff68b51518fb
Sarita <saritaradia65@gmail.com>
2022-12-09 15:23:20 +0000

    Green test Adds 55, and returns a string confirming 55 has been added

--- new-practice/02-testing-classes/spec/counter_spec.rb
+++ new-practice/02-testing-classes/spec/counter_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe 'counter' do
         expect(counter.report).to eq("Counted to 1 so far.")
     end 
 
+    it "Adds 55, and returns a string confirming 55 has been added" do
+        counter = Counter.new 
+        counter.add(55)
+        expect(counter.report).to eq("Counted to 55 so far.")
+    end 
+
 end 
