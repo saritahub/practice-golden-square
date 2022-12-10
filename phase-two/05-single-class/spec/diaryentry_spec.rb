359502e75701431b890b61069f8e2ec6d1832f37
Sarita <saritaradia65@gmail.com>
2022-12-10 15:37:07 +0000

    Green test: Returns a string as the title

--- phase-two/05-single-class/spec/diaryentry_spec.rb
+++ phase-two/05-single-class/spec/diaryentry_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe "DiaryEntry" do
             diaryentry = DiaryEntry.new("", "The contents")
             expect(diaryentry.title).to eq("")
         end 
+
+        it "Returns a string as the title" do 
+            diaryentry = DiaryEntry.new("The title", "The contents")
+            expect(diaryentry.title).to eq("The title")
+        end 
     end 
 
 end 
