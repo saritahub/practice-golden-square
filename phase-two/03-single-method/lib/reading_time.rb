370fd989da1c9470b2625e471ba597723b47179d
Sarita <saritaradia65@gmail.com>
2022-12-10 13:02:40 +0000

    Refactor

--- phase-two/03-single-method/lib/reading_time.rb
+++ phase-two/03-single-method/lib/reading_time.rb
@@ -2,5 +2,5 @@ def reading_time(text)
     fail "Please input a string." if text.class != String 
     text_length = text.split(" ").length 
 
-    return (text_length.to_f / 200).ceil
+    (text_length.to_f / 200).ceil
 end 
