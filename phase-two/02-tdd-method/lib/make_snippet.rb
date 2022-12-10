77cd5da2bb252ade2bc67e57187b0a72228bfb1e
Sarita <saritaradia65@gmail.com>
2022-12-10 12:02:09 +0000

    Refactored snippet

--- phase-two/02-tdd-method/lib/make_snippet.rb
+++ phase-two/02-tdd-method/lib/make_snippet.rb
@@ -1,12 +1,8 @@
-#  make_snippet takes a string argument
-#  and returns the first five words and then a '...' 
-#  if there are more than that.
-
 def make_snippet(sentence)
     if sentence.split(" ").length <= 5
         return sentence
     else 
         snippet = sentence.split(" ")[0..4].join(" ")
-        return snippet + "..."
+        return "#{snippet}..."
     end 
 end 
