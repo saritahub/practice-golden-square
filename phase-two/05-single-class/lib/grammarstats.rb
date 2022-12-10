368f57da18082e3ca22d7169a7296f6cb50a2e6b
Sarita <saritaradia65@gmail.com>
2022-12-10 20:19:41 +0000

    Refactor

--- phase-two/05-single-class/lib/grammarstats.rb
+++ phase-two/05-single-class/lib/grammarstats.rb
@@ -10,10 +10,15 @@ class GrammarStats
         first_char = text[0].upcase == text[0]
         last_char = text[-1] == "!"
 
-        first_char && last_char ?  @passing_text.push(text) && true : false 
+        first_char && last_char ? @passing_text.push(text) && true : false 
     end
   
     def percentage_good
-        !@all_text.empty? && !@passing_text.empty? ? ((@passing_text.length.to_f / @all_text.length)*100).ceil : 0
+        !@all_text.empty? && !@passing_text.empty? ? calculate_percentage : 0
     end
+
+    private
+    def calculate_percentage
+        ((@passing_text.length.to_f / @all_text.length)*100).ceil 
+    end 
 end
