1be89dbfd5225585ae34927e8ab67c8b0790bfe6
Sarita <saritaradia65@gmail.com>
2022-12-10 13:43:46 +0000

    Green test: Checks if the sentence starts with a capital letter and ends with a punctuation mark

--- phase-two/03-single-method/spec/grammar_check_spec.rb
+++ phase-two/03-single-method/spec/grammar_check_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe "grammar_check" do
         it "Checks if the sentence ends with a punctuation mark" do
             expect(grammar_check("Hey")).to eq(true) 
         end 
+
+        it "Checks if the sentence starts with a capital letter and ends with a punctuation mark " do
+            expect(grammar_check("Hey")).to eq(true) 
+        end 
     end 
 end 
