bec0dfea1bbc567539c1b82c9998eb7d90243388
Sarita <saritaradia65@gmail.com>
2022-12-10 12:05:49 +0000

    Green test: Returns an empty string

--- phase-two/02-tdd-method/spec/make_snippet_spec.rb
+++ phase-two/02-tdd-method/spec/make_snippet_spec.rb
@@ -1,6 +1,10 @@
 require 'make_snippet'
 
 RSpec.describe "make_snippet" do 
+    it "Returns an empty string" do 
+        expect(make_snippet("")).to eq("")
+
+    end 
     context "Returns the string if there are 5 words or less" do 
         it "Returns the string if there are 2 words" do 
            expect(make_snippet("Baked beans")).to eq("Baked beans")
