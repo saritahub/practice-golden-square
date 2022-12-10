870affcbe2cb3c61ee16dc77cd4679a47c8d5f51
Sarita <saritaradia65@gmail.com>
2022-12-10 11:54:16 +0000

    Green test: Returns the string if there are 5 words

--- phase-two/02-tdd-method/spec/make_snippet_spec.rb
+++ phase-two/02-tdd-method/spec/make_snippet_spec.rb
@@ -2,9 +2,13 @@ require 'make_snippet'
 
 RSpec.describe "make_snippet" do 
     context "Returns the string if there are 5 words or less" do 
-        it "Returns the string if there are 5 words" do 
+        it "Returns the string if there are 2 words" do 
            expect(make_snippet("Baked beans")).to eq("Baked beans")
         end 
+
+        it "Returns the string if there are 5 words" do 
+           expect(make_snippet("There are five words here.")).to eq("There are five words here.")
+        end 
     end 
-    
+
 end 
