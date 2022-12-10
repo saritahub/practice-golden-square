73e19e4eb8028bf9dab3234169582dbefc9de7eb
Sarita <saritaradia65@gmail.com>
2022-12-10 20:24:45 +0000

    green test: adds two tasks to the list, and returns the task list

--- phase-two/06-single-class/spec/todolist_spec.rb
+++ phase-two/06-single-class/spec/todolist_spec.rb
@@ -14,5 +14,12 @@ RSpec.describe ToDoList do
             todolist.add("Walk the dog")
             expect(todolist.task_list).to eq("Walk the dog")
         end 
+
+        it "Adds two tasks to the list, and returns the task list" do 
+            todolist = ToDoList.new
+            todolist.add("Walk the dog")
+            todolist.add("Wash the car")
+            expect(todolist.task_list).to eq("Walk the dog, Wash the car")
+        end 
     end 
 end
