a3488ab2884df022a846e66c124bfc6b70bd4c1f
Sarita <saritaradia65@gmail.com>
2022-12-22 16:01:45 +0000

    Green test: Longer entry: Constructs a diary entry, and returns the number of words in contents

--- phase-two/08-multi-class/spec/diary_entry_spec.rb
+++ phase-two/08-multi-class/spec/diary_entry_spec.rb
@@ -11,5 +11,10 @@ RSpec.describe "Diary Entry" do
     it 'Constructs a diary entry, and returns the number of words in contents' do
         diaryentry = DiaryEntry.new("The title", "The contents")
         expect(diaryentry.count_words).to eq(2)
-      end 
+    end 
+
+    it 'Longer entry: Constructs a diary entry, and returns the number of words in contents' do
+        diaryentry = DiaryEntry.new("The title", "Longer passage " * 20)
+        expect(diaryentry.count_words).to eq(40)
+    end
 end 
