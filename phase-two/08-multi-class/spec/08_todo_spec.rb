65f00a86057e903fdfca2e14348a372c81c3517f
Sarita <saritaradia65@gmail.com>
2022-12-23 16:32:08 +0000

    green test: Constructs todo tasks and returns them as a string

--- phase-two/08-multi-class/spec/08_todo_spec.rb
+++ phase-two/08-multi-class/spec/08_todo_spec.rb
@@ -7,10 +7,11 @@ RSpec.describe "ToDo" do
             expect(todo.task).to eq('Go for a run')
         end
         
-        xit 'Constructs todo tasks and returns them as a string' do
+        it 'Constructs todo tasks and returns them as a string' do
             todo_1 = ToDo.new('Go for a run')
             todo_2 = ToDo.new('Walk the dog')
-            expect(todo.task).to eq('Go for a run, Walk the dog')
+            expect(todo_1.task).to eq('Go for a run')
+            expect(todo_2.task).to eq('Walk the dog')
         end 
     
     end
