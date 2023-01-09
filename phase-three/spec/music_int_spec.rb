302d0fa4d7f184224f06c81f9a7804b51ba8cdbd
Sarita <saritaradia65@gmail.com>
2023-01-09 16:06:52 +0000

    green test: Adds two tracks, and returns them

--- phase-three/spec/music_int_spec.rb
+++ phase-three/spec/music_int_spec.rb
@@ -11,5 +11,14 @@ RSpec.describe 'Music Library Integration' do
             music_library.add(track)
             expect(music_library.all).to eq([track])
         end 
+
+        it 'Adds two tracks, and returns them' do 
+            track_1 = Track.new('Halo', 'Beyonce')
+            track_2 = Track.new('Hello', 'Adele')
+            music_library = MusicLibrary.new 
+            music_library.add(track_1)
+            music_library.add(track_2)
+            expect(music_library.all).to eq([track_1, track_2])
+        end 
     end 
 end 
