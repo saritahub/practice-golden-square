639b3de6aaea013e04878c37ff61ae5d30428096
Sarita <saritaradia65@gmail.com>
2023-01-10 11:34:38 +0000

    green test: Doubles: Adds one track, and returns this

--- phase-three/spec/music_lib_spec.rb
+++ phase-three/spec/music_lib_spec.rb
@@ -3,6 +3,13 @@
 require 'music_library'
 
 RSpec.describe 'Music Library' do 
-    context 'Using doubles to check expected behavior' do 
+    context 'Using doubles to check expected behavior' do
+        it 'Doubles: Adds one track, and returns this' do 
+            track_double = (double(:fake_track, title: 'Halo', artist: 'Beyonce'))
+            music_library = MusicLibrary.new 
+            music_library.add(track_double)
+            expect(music_library.all).to eq([track_double])
+        end 
+
     end 
 end 
