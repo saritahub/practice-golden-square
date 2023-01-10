82c928701a915893cb3f43a94fa4715acef1c378
Sarita <saritaradia65@gmail.com>
2023-01-10 12:25:21 +0000

    green test: Double: Adds tasks to a list"

--- codebases/mocking_bites/spec/task_list_spec.rb
+++ codebases/mocking_bites/spec/task_list_spec.rb
@@ -12,4 +12,13 @@ RSpec.describe TaskList do
   end
 
   # Unit test `#all` and `#all_complete?` behaviour
+  
+  it "Double: Adds tasks to a list" do
+    task_list = TaskList.new
+    double_task_1 = double(:fake_task, title: 'Walk the dog')
+    double_task_2 = double(:fake_task, title: 'Hike')
+    task_list.add(double_task_1)
+    task_list.add(double_task_2)
+    expect(task_list.all).to eq [double_task_1, double_task_2]
+  end
 end
