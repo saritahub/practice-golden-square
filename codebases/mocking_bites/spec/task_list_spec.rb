efb6853c756b8ffd2fdba7a6d3d2d033f348eb07
Sarita <saritaradia65@gmail.com>
2023-01-10 13:10:08 +0000

    green testL Double: arks tasks as complete

--- codebases/mocking_bites/spec/task_list_spec.rb
+++ codebases/mocking_bites/spec/task_list_spec.rb
@@ -11,8 +11,6 @@ RSpec.describe TaskList do
     expect(task_list.all_complete?).to eq false
   end
 
-  # Unit test `#all` and `#all_complete?` behaviour
-  
   it "Double: Adds tasks to a list" do
     task_list = TaskList.new
     double_task_1 = double(:fake_task, title: 'Walk the dog')
@@ -20,5 +18,16 @@ RSpec.describe TaskList do
     task_list.add(double_task_1)
     task_list.add(double_task_2)
     expect(task_list.all).to eq [double_task_1, double_task_2]
+  end
+
+  it "Double: Marks tasks as complete" do
+    task_list = TaskList.new
+    double_task_1 = double(:fake_task, title: 'Walk the dog', mark_complete: true)
+    double_task_2 = double(:fake_task, title: 'Hike', mark_complete: true)
+    task_list.add(double_task_1)
+    task_list.add(double_task_2)
+    expect(double_task_1).to receive(:complete?).and_return(true)
+    expect(double_task_2).to receive(:complete?).and_return(true)
+    expect(task_list.all_complete?).to eq true
   end
 end
