bacdd26fbf0e2218e2a0f5cc6b959234c8133d40
Sarita <saritaradia65@gmail.com>
2023-01-20 14:32:41 +0000

    removed locked from diary double

--- phase-three/spec/secret_diary_spec.rb
+++ phase-three/spec/secret_diary_spec.rb
@@ -4,9 +4,15 @@ require 'secret_diary'
 
 RSpec.describe 'Mocked Diary for Secret Diary Integration' do 
     it 'Mock: Returns Go Away! if the diary is locked' do 
-        mock_diary = double(:mock_diary, contents: 'Cake', locked: true)
+        mock_diary = double(:mock_diary, contents: 'Cake')
         secret_diary = SecretDiary.new(mock_diary)
         expect(secret_diary.read).to eq('Go away!')
+    end 
+
+    xit 'Returns diary contents if the diary is locked' do 
+        mock_diary = double(:mock_diary, contents: 'Walk in the dark', locked: false)
+        secret_diary = SecretDiary.new(mock_diary)
+        expect(secret_diary.read).to eq('Walk in the dark')
     end 
 
 end 
