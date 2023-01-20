829d16ae9f82690395b9f6a8ef528d23aabb32cf
Sarita <saritaradia65@gmail.com>
2023-01-20 13:59:49 +0000

    Refactor Diary Entry using Struct.new

--- phase-three/lib/diary_entry.rb
+++ phase-three/lib/diary_entry.rb
@@ -1,16 +1,4 @@
 # File: lib/diary_entry.rb
 
-class DiaryEntry
-    def initialize(title, contents) 
-        @title = title
-        @contents = contents
-    end
-  
-    def title
-      @title
-    end
-  
-    def contents
-      @contents
-    end
-  end
+class DiaryEntry < Struct.new(:title, :contents)
+end
